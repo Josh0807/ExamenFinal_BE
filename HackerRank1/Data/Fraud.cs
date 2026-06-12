fbe1ecb04fc19a71e82b6d39f77b60d26d072931
Josh0807 <joshuadiaz0807@gmail.com>
2026-06-12 01:18:03 -0600

    Cambios como el Fraud

---
+++ HackerRank1/Data/Fraud.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryService.WebAPI.Data
+{
+    public class Fraud
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string ImpostorDetails { get; set; }
+
+        [Required]
+        public string ContactInfo { get; set; }
+
+        public string Comments { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
